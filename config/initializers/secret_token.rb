c39ded5ccfe7898a8a6def36cf7b3361f9c028b1
goombapolice <your.goomba_police@yahoo.com>
2013-05-14 22:43:07 -0400

    Test repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9df9cd0ae1db729b1cdb0b6532fcaf984a120207f65fbbbdf82d2ae0217e83cbe21c2520fc1cbc8c5b0099199a12e8f2f7075d6027c5fdec805ca54e24d0193d'
